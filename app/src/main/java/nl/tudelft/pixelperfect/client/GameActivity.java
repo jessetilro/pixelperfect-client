b36bc7ebbddb3c6de3cf328aba2a70e81f0579ce
Jesse Tilro <jesse@jessetilro.nl>
2016-05-10 17:05:13 +0200

    Add GUI options for connecting.
    Switched to a default Android Activity class rather than the jMonkeyEngine
    AndroidHarness, simply because it is a giant mess in my opinion. Used the
    networking functionalities of the jMonkeyEngine in a creative way (from an
    Android AsyncTask) and added GUI options to enter IP Address and connect.

---
+++ app/src/main/java/nl/tudelft/pixelperfect/client/GameActivity.java
@@ -0,0 +1,8 @@
+package nl.tudelft.pixelperfect.client;
+
+/**
+ * Created by jesse on 10-5-2016.
+ */
+public interface GameActivity {
+    void showMessage(CharSequence text);
+}
--- app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/MainActivity.java
+++ app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/MainActivity.java
@@ -1,13 +1,47 @@
 package nl.tudelft.pixelperfect.pixelperfect;
 
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.jme3.network.Client;
+import com.jme3.system.JmeContext;
+
+import nl.tudelft.pixelperfect.client.GameActivity;
+import nl.tudelft.pixelperfect.client.GameClient;
+
+public class MainActivity extends AppCompatActivity implements GameActivity {
+
+    private GameClient game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        game = GameClient.getInstance();
+    }
+
+    public void connect(View view) {
+        EditText text = (EditText) findViewById(R.id.ip_address);
+        String ip = text.getText().toString();
+        System.out.println("Connecting to: " + ip);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Trying to establish a connection with the game server at '" + ip + "'.")
+                .setTitle("Connecting");
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        game.connectToServer(ip);
+    }
+
+    public void showMessage(CharSequence text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
