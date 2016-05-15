107ae7501ff1f1e2be703047d763d0bb1581b21a
Floris Doolaard <F.P.Doolaard@student.tudelft.nl>
2016-05-15 20:40:20 +0200

    Added a button.
    The button should send a message to the main game. (not implented yet)

--- app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/GameActivity.java
+++ app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/GameActivity.java
@@ -2,12 +2,22 @@ package nl.tudelft.pixelperfect.pixelperfect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Button button = (Button) findViewById(R.id.button_complete_event);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
