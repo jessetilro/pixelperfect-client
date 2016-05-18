17063ebca234fe72fb1b0d20123f63fc3df8e522
Dmitry Malarev <D.R.Malarev@student.tudelft.nl>
2016-05-18 19:50:45 +0200

    Added missing Serializer and MessageListener registrations

--- app/src/main/java/nl/tudelft/pixelperfect/client/ConnectTask.java
+++ app/src/main/java/nl/tudelft/pixelperfect/client/ConnectTask.java
@@ -22,9 +22,11 @@ public class ConnectTask extends AsyncTask<String, Void, Client> {
             client = Network.connectToServer(ip[0], 6143);
             Serializer.registerClass(HelloMessage.class);
             Serializer.registerClass(EventCompletedMessage.class);
+            Serializer.registerClass(EventsMessage.class);
             client.start();
             client.addMessageListener(new ClientListener(), HelloMessage.class);
             client.addMessageListener(new ClientListener(), EventCompletedMessage.class);
+            client.addMessageListener(new ClientListener(), EventsMessage.class);
         } catch (IOException e) {
             client = null;
         }
