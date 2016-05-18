58f22408544c7344fcffe00424f9de35566389bb
Dmitry Malarev <D.R.Malarev@student.tudelft.nl>
2016-05-18 15:25:07 +0200

    Added switch state to categorise message in the right event subclass.

--- app/src/main/java/nl/tudelft/pixelperfect/client/ClientListener.java
+++ app/src/main/java/nl/tudelft/pixelperfect/client/ClientListener.java
@@ -4,8 +4,14 @@ import com.jme3.network.Client;
 import com.jme3.network.Message;
 import com.jme3.network.MessageListener;
 
+import nl.tudelft.pixelperfect.event.AsteroidFieldEvent;
+import nl.tudelft.pixelperfect.event.Event;
+import nl.tudelft.pixelperfect.event.FireEvent;
+import nl.tudelft.pixelperfect.event.HostileShipEvent;
+import nl.tudelft.pixelperfect.event.PlasmaLeakEvent;
+
 /**
- * The ClientListenere waits for incoming messages from the server and interps them.
+ * The ClientListener waits for incoming messages from the server and interps them.
  *
  * @author Jesse Tilro
  * @author Floris Doolaard
@@ -17,9 +23,28 @@ public class ClientListener implements MessageListener<Client> {
             HelloMessage helloMessage = (HelloMessage) message;
             System.out.println("Client #"+source.getId()+" received: '"+helloMessage.getSomething()+"'");
         } else if (message instanceof EventsMessage) {
+            Event mission;
             EventsMessage eve = (EventsMessage) message;
             System.out.println("Client #"+source.getId()+" received event: '"+eve.getEvent()+"'");
             String[] keys = eve.getEvent().split(" ");
+            switch (keys[1]) {
+                case "FireEvent":
+                    mission = new FireEvent(Integer.getInteger(keys[0]), "", "", Long.getLong(keys[2]), Long.getLong(keys[3]), 0);
+                    break;
+                case "AsteroidFieldEvent":
+                    mission = new AsteroidFieldEvent(Integer.getInteger(keys[0]), "", "", Long.getLong(keys[2]), Long.getLong(keys[3]), 0);
+                    break;
+                case "HostileShipEvent":
+                    mission = new HostileShipEvent(Integer.getInteger(keys[0]), "", "", Long.getLong(keys[2]), Long.getLong(keys[3]), 0);
+                    break;
+                case "PlasmaLeakEvent":
+                    mission = new PlasmaLeakEvent(Integer.getInteger(keys[0]), "", "", Long.getLong(keys[2]), Long.getLong(keys[3]), 0);
+                    break;
+                default:
+                    mission = null;
+                    break;
+            }
+
         }
     }
 }
