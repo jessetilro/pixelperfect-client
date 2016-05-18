fdb06b024b5dc83da3db401af2f02c6cb289109e
Dmitry Malarev <D.R.Malarev@student.tudelft.nl>
2016-05-18 23:09:23 +0200

    Changed the message

--- app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/GameActivity.java
+++ app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/GameActivity.java
@@ -40,7 +40,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
-        EventCompletedMessage eventCompleted = new EventCompletedMessage("Hello Message!");
+        EventCompletedMessage eventCompleted = new EventCompletedMessage("0");
         game.sendMessage(eventCompleted);
     }
 }
