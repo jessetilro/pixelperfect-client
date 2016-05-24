fa8a955631d3728cdf82a79a69212b5611805c94
Floris Doolaard <F.P.Doolaard@student.tudelft.nl>
2016-05-24 10:57:46 +0200

    Created new role activity.
    Still without functionality.

---
+++ app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/RoleActivity.java
@@ -0,0 +1,25 @@
+package nl.tudelft.pixelperfect.pixelperfect;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * This Activity involves the allocation of roles. Each player is able to choose a unique
+ * role in this activity.
+ *
+ * @author Floris Doolaard
+ */
+public class RoleActivity extends AppCompatActivity {
+
+    /**
+     * This method shows what happens when this Activity is created.
+     *
+     * @param savedInstanceState , a Bundle.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
