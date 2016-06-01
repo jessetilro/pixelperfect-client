f9de79f946f01f1f4e1b05ee4de8db86dc1cb237
Floris Doolaard <floris.do@gmail.com>
2016-06-01 13:02:09 +0200

    Manually testing progressBar and SeekBar.

--- app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/mini_game/AsteroidImpactActivity.java
+++ app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/mini_game/AsteroidImpactActivity.java
@@ -3,6 +3,7 @@ package nl.tudelft.pixelperfect.pixelperfect.mini_game;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 import nl.tudelft.pixelperfect.pixelperfect.R;
 
@@ -14,14 +15,18 @@ import nl.tudelft.pixelperfect.pixelperfect.R;
  */
 public class AsteroidImpactActivity extends AppCompatActivity  {
     private ProgressBar progressBar;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mini_game_asteroid_impact);
 
-        progressBar = (ProgressBar) findViewById(R.id.mini_game_progress_bar);
-        progressBar.incrementProgressBy(50);
+        progressBar = (ProgressBar) findViewById(R.id.mini_game_asteroid_impact_progressBar);
+        seekBar = (SeekBar) findViewById(R.id.mini_game_asteroid_impact_seekBar);
+
+        progressBar.incrementProgressBy(99);
+        seekBar.incrementProgressBy(40);
     }
 
 }
