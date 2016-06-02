0ebf40e7f6f33c4051344d488faf90ba4a00885f
wouterz <zirkzee@hotmail.com>
2016-06-02 21:47:01 +0200

    refactoring a bit

--- app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/mini_game/CoffeeBoostActivity.java
+++ app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/mini_game/CoffeeBoostActivity.java
@@ -7,11 +7,9 @@ import java.util.Collections;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AbsoluteLayout;
 import android.widget.Button;
-import android.widget.GridLayout;
 
 import nl.tudelft.pixelperfect.pixelperfect.R;
 
@@ -28,10 +26,8 @@ public class CoffeeBoostActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coffee_boost);
-//
-//        movable = false;
-        initialize();
 
+        initialize();
         draw();
     }
 
@@ -61,61 +57,58 @@ public class CoffeeBoostActivity extends Activity {
         }
     }
 
-    private void makeMove(final Button b) {
-        movable = true;
-        int number, position, emptyspace_position;
-        number = Integer.parseInt((String) b.getText());
-        position = current_order.indexOf(number);
-        emptyspace_position = current_order.indexOf(0);
-        switch(emptyspace_position) {
+    private void makeMove(final Button clicked) {
+        movable = false;
+        int number = Integer.parseInt((String) clicked.getText());
+        int position = current_order.indexOf(number);
+        int empty_space_position = current_order.indexOf(0);
+        switch(empty_space_position) {
             case(0):
                 if (position == 1 || position == 3)
-                    movable = false;
+                    movable = true;
                 break;
             case(1):
                 if (position == 0 || position == 2 || position == 4)
-                    movable = false;
+                    movable = true;
                 break;
             case(2):
                 if (position == 1 || position == 5)
-                    movable = false;
+                    movable = true;
                 break;
             case(3):
                 if (position == 0 || position == 4 || position == 6)
-                    movable = false;
+                    movable = true;
                 break;
             case(4):
                 if (position == 1 || position == 3 || position == 5 || position == 7)
-                    movable = false;
+                    movable = true;
                 break;
             case(5):
                 if (position == 2 || position == 4 || position == 8)
-                    movable = false;
+                    movable = true;
                 break;
             case(6):
                 if (position == 3 || position == 7)
-                    movable = false;
+                    movable = true;
                 break;
             case(7):
                 if (position == 4 || position == 6 || position == 8)
-                    movable = false;
+                    movable = true;
                 break;
             case(8):
                 if (position == 5 || position == 7)
-                    movable = false;
+                    movable = true;
                 break;
         }
 
         if (movable) {
-            return;
+            current_order.remove(position);
+            current_order.add(position, 0);
+            current_order.remove(empty_space_position);
+            current_order.add(empty_space_position, number);
+
+            draw();
         }
-
-        current_order.remove(position);
-        current_order.add(position, 0);
-        current_order.remove(emptyspace_position);
-        current_order.add(emptyspace_position,number);
-
-        draw();
     }
 
     public boolean gameComplete() {
@@ -131,47 +124,44 @@ public class CoffeeBoostActivity extends Activity {
     private void draw() {
         for (int i = 0; i < 9; i++) {
             int original_location = current_order.get(i);
-
             AbsoluteLayout.LayoutParams layout =
                     (AbsoluteLayout.LayoutParams) buttons[original_location].getLayoutParams();
-//            GridLayout.LayoutParams grid = (GridLayout.LayoutParams) buttons[original_location].getLayoutParams();
-//            grid.rowSpec.toString();
             switch(i) {
                 case(0):
-                    layout.x = 5;
-                    layout.y = 5;
+                    layout.x = 0;
+                    layout.y = 0;
                     break;
                 case(1):
-                    layout.x = 110;
-                    layout.y = 5;
+                    layout.x = 200;
+                    layout.y = 0;
                     break;
                 case(2):
-                    layout.x = 215;
-                    layout.y = 5;
+                    layout.x = 400;
+                    layout.y = 0;
                     break;
                 case(3):
-                    layout.x = 5;
-                    layout.y = 110;
+                    layout.x = 0;
+                    layout.y = 200;
                     break;
                 case(4):
-                    layout.x =110;
-                    layout.y =110;
+                    layout.x = 200;
+                    layout.y = 200;
                     break;
                 case(5):
-                    layout.x = 215;
-                    layout.y =110;
+                    layout.x = 400;
+                    layout.y = 200;
                     break;
                 case(6):
-                    layout.x = 5;
-                    layout.y = 215;
+                    layout.x = 0;
+                    layout.y = 400;
                     break;
                 case(7):
-                    layout.x = 110;
-                    layout.y = 215;
+                    layout.x = 200;
+                    layout.y = 400;
                     break;
                 case(8):
-                    layout.x = 215;
-                    layout.y = 215;
+                    layout.x = 400;
+                    layout.y = 400;
                     break;
 
             }
