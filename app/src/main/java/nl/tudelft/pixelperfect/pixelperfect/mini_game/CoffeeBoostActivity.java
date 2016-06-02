0ded341df8d8633beac856b355ba243c08bea031
wouterz <zirkzee@hotmail.com>
2016-06-03 00:12:43 +0200

    javadoc and bit of refactoring

--- app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/mini_game/CoffeeBoostActivity.java
+++ app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/mini_game/CoffeeBoostActivity.java
@@ -1,7 +1,5 @@
 package nl.tudelft.pixelperfect.pixelperfect.mini_game;
 
-
-
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -17,57 +15,62 @@ import nl.tudelft.pixelperfect.event.Events;
 import nl.tudelft.pixelperfect.pixelperfect.R;
 import nl.tudelft.pixelperfect.pixelperfect.Spaceship;
 
+/**
+ * Mini-game for the Janitor.
+ *
+ * @author Wouter Zirkzee
+ *
+ */
 public class CoffeeBoostActivity extends Activity {
-    private GameClient game = GameClient.getInstance();
+
+    private final GameClient game = GameClient.getInstance();
     private static Spaceship ship = Spaceship.getInstance();
     private Button[] buttons = new Button[9];
     private static final Integer[] original_order = new Integer[] {0, 1, 2, 3, 4, 5, 6, 7, 8};
-    private ArrayList<Integer> current_order = new ArrayList<Integer>();
-
     private GridLayout gridLayout;
+    private ArrayList<Integer> current_order = new ArrayList<Integer>();
+    private ArrayList<GridLayout.Spec> rows = new ArrayList<GridLayout.Spec>();
+    private ArrayList<GridLayout.Spec> columns = new ArrayList<GridLayout.Spec>();
 
-    ArrayList<GridLayout.Spec> rows = new ArrayList<GridLayout.Spec>();
-    ArrayList<GridLayout.Spec> columns = new ArrayList<GridLayout.Spec>();
 
-
+    /**
+     * This method is for when this activity is started.
+     *
+     * @param savedInstanceState
+     *                      A bundle.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coffee_boost);
         initialize();
-        gridLayout = (GridLayout) findViewById(R.id.gridLayout);
 
-        GridLayout.Spec row1 = GridLayout.spec(0);
-        GridLayout.Spec row2 = GridLayout.spec(1);
-        GridLayout.Spec row3 = GridLayout.spec(2);
-        GridLayout.Spec col1 = GridLayout.spec(0);
-        GridLayout.Spec col2 = GridLayout.spec(1);
-        GridLayout.Spec col3 = GridLayout.spec(2);
-        rows.add(row1); rows.add(row2); rows.add(row3);
-        columns.add(col1); columns.add(col2); columns.add(col3);
-        shuffle();
+
     }
 
+    /**
+     * Shuffle the game before starting.
+     */
     private void shuffle() {
         Random random = new Random();
         random.setSeed(System.currentTimeMillis());
-        int limit = random.nextInt(10);
+        int limit = random.nextInt(10) + 5;
         for (int i = 0; i < limit; i++) {
             makeMove(buttons[random.nextInt(7)+1]);
         }
     }
 
+    /**
+     * Initialize some aspects of this game.
+     */
     private void initialize() {
-        buttons[0] = (Button) findViewById(R.id.button1);
-        buttons[1] = (Button) findViewById(R.id.button2);
-        buttons[2] = (Button) findViewById(R.id.button3);
-        buttons[3] = (Button) findViewById(R.id.button4);
-        buttons[4] = (Button) findViewById(R.id.button5);
-        buttons[5] = (Button) findViewById(R.id.button6);
-        buttons[6] = (Button) findViewById(R.id.button7);
-        buttons[7] = (Button) findViewById(R.id.button8);
-        buttons[8] = (Button) findViewById(R.id.button9);
-
+        gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+        int[] buttonsIds = {R.id.button1, R.id.button2, R.id.button3,
+                R.id.button4, R.id.button5, R.id.button6, R.id.button7,
+                R.id.button8, R.id.button9};
+        for (int i = 0; i < buttonsIds.length; i++) {
+            buttons[i] = (Button) findViewById(buttonsIds[i]);
+        }
         for(int i = 0; i < 9; i++) {
             current_order.add(i);
         }
@@ -81,8 +84,23 @@ public class CoffeeBoostActivity extends Activity {
             );
         }
 
+        GridLayout.Spec row1 = GridLayout.spec(0);
+        GridLayout.Spec row2 = GridLayout.spec(1);
+        GridLayout.Spec row3 = GridLayout.spec(2);
+        GridLayout.Spec col1 = GridLayout.spec(0);
+        GridLayout.Spec col2 = GridLayout.spec(1);
+        GridLayout.Spec col3 = GridLayout.spec(2);
+        rows.add(row1); rows.add(row2); rows.add(row3);
+        columns.add(col1); columns.add(col2); columns.add(col3);
+        shuffle();
     }
 
+    /**
+     * Swap buttons if they are correctly positioned.
+     *
+     * @param clicked
+     *              The clicked button.
+     */
     private void makeMove(final Button clicked) {
         int number = Integer.parseInt((String) clicked.getText())-1;
         int position = current_order.indexOf(number);
@@ -107,21 +125,39 @@ public class CoffeeBoostActivity extends Activity {
 
     }
 
+    /**
+     * Check if the buttons are in the same row or column.
+     *
+     * @param loc1
+     *              Location of the first button.
+     * @param loc2
+     *             Location of the second button.
+     * @return True if they are in the same row or column.
+     */
     private boolean sameRowOrColumn(int loc1, int loc2) {
-        if ((loc1/3) == (loc2/3) || (loc1%3) == (loc2%3)) {
-            return true;
-        }
-        return false;
-    }
-    private boolean areNeighbours(int loc1, int loc2) {
-        if ( (Math.abs((loc1/3) - (loc2/3)) == 1)
-                || Math.abs((loc1%3) - (loc2%3))  == 1 ) {
-            return true;
-        }
-        return false;
+        return ((loc1/3) == (loc2/3) || (loc1%3) == (loc2%3));
     }
 
-    public boolean gameComplete() {
+    /**
+     * Check if the tapped button is next to the empty space.
+     *
+     * @param loc1
+     *              Location of the first button.
+     * @param loc2
+     *             Location of the second button.
+     * @return True if the distance is 1 in any direction.
+     */
+    private boolean areNeighbours(int loc1, int loc2) {
+        return ((Math.abs((loc1/3) - (loc2/3)) == 1)
+        || Math.abs((loc1%3) - (loc2%3))  == 1);
+    }
+
+    /**
+     * Check if the game is completed.
+     *
+     * @return True if completed, otherwise false.
+     */
+    private boolean gameComplete() {
         for(int i = 0; i < 9; i++) {
             if (!current_order.get(i).equals(original_order[i])) {
                 return false;
@@ -130,8 +166,10 @@ public class CoffeeBoostActivity extends Activity {
         return true;
     }
 
-
-    public void confirmCoffeeEvent(View view) {
+    /**
+     * Check if the game is complete and send a message.
+     */
+    public void confirmCoffeeEvent() {
         if (gameComplete()) {
             game.sendMessage(new EventCompletedMessage("Coffee Event", ship.getEventLog().pop(Events.COFFEE).getId()));
         }
