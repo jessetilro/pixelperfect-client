d33b239fbfdd8cb9c5082a60faf6a9ded4a10a74
Floris Doolaard <floris.do@gmail.com>
2016-06-09 20:26:47 +0200

    Interactively transition to the location of the chosen role.

--- app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/LobbyActivity.java
+++ app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/LobbyActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import nl.tudelft.pixelperfect.client.GameClient;
+import nl.tudelft.pixelperfect.game.Roles;
+import nl.tudelft.pixelperfect.pixelperfect.location.LocationArmoryActivity;
+import nl.tudelft.pixelperfect.pixelperfect.location.LocationDeckActivity;
+import nl.tudelft.pixelperfect.pixelperfect.location.LocationEngineroomActivity;
+import nl.tudelft.pixelperfect.pixelperfect.location.LocationLabActivity;
 
 /**
  * The lobby is where the client user connects to the game and waits for the captain to start it.
@@ -14,6 +19,7 @@ import nl.tudelft.pixelperfect.client.GameClient;
  */
 public class LobbyActivity extends AppCompatActivity {
     private static Context mContext;
+    private static Roles chosenRole;
 
     /**
      * Whenever this activity is created the layout will be set.
@@ -26,13 +32,33 @@ public class LobbyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lobby);
 
         mContext = this;
+        chosenRole = (Roles) getIntent().getSerializableExtra("Role");
     }
 
     /**
      * Starts the game because a message was received from the server.
      */
     public static void startGame() {
-        Intent newGame = new Intent(mContext, RoleActivity.class);
+        Intent newGame;
+
+        switch(chosenRole){
+            case GUNNER:
+                newGame = new Intent(mContext, LocationArmoryActivity.class);
+                break;
+            case ENGINEER:
+                newGame = new Intent(mContext, LocationEngineroomActivity.class);
+                break;
+            case SCIENTIST:
+                newGame = new Intent(mContext, LocationLabActivity.class);
+                break;
+            case JANITOR:
+                newGame = new Intent(mContext, LocationDeckActivity.class);
+                break;
+            default:
+                System.out.println("Error: something went wrong trying to select a role.");
+                newGame = new Intent(mContext, MainActivity.class);
+                break;
+        }
         mContext.startActivity(newGame);
     }
 }
