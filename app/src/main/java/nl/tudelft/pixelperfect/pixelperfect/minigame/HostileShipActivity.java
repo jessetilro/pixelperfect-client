0ec555d95f9a3eea5276a9782aad5f5ab883ab33
Jesse Tilro <jesse@jessetilro.nl>
2016-06-09 23:08:01 +0200

    Add Hostile Ship minigame.
    Implemented the functionality and layout for the Hostile Ship minigame.

--- app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/minigame/HostileShipActivity.java
+++ app/src/main/java/nl/tudelft/pixelperfect/pixelperfect/minigame/HostileShipActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +24,7 @@ import nl.tudelft.pixelperfect.pixelperfect.location.LocationArmoryActivity;
  */
 public class HostileShipActivity extends AppCompatActivity {
     private GameClient game = GameClient.getInstance();
+    private int armor;
 
     /**
      * Initializing elements when the activity is created.
@@ -31,19 +35,54 @@ public class HostileShipActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minigame_hostile_ship);
+        armor = 0;
+    }
+
+    /**
+     * Handler called when the selection for the armor field is updated.
+     * @param view The view element.
+     */
+    public void onSelectArmor(View view) {
+        int[] ids = { R.id.armor_energy_shield, R.id.armor_graphene, R.id.armor_titanium };
+        for (int index = 0; index < ids.length; index++) {
+            if (ids[index] == view.getId()) {
+                armor = index;
+            }
+        }
+    }
+
+    /**
+     * Read the entered x-coordinate from the view.
+     * @return The entered position x parameter value.
+     */
+    private int readPositionX() {
+        EditText positionX = (EditText) findViewById(R.id.position_x);
+        return Integer.parseInt(positionX.getText().toString());
+    }
+
+    /**
+     * Read an entered integer value from the view.
+     * @return The entered integer value.
+     */
+    private int readInteger(int id) {
+        EditText editText = (EditText) findViewById(id);
+        return Integer.parseInt(editText.getText().toString());
     }
 
     /**
      * If the event is completed, send the message to the server.
      *
-     * @param view, the view of the page.
+     * @param view The view element.
      */
     public void complete(View view) {
         EventCompletedMessage message = new EventCompletedMessage(EventTypes.HOSTILE_SHIP.ordinal());
         Map<String, Integer> parameters = new HashMap<String, Integer>();
-        parameters.put("testParam", 42);
+        parameters.put("positionX", readInteger(R.id.position_x));
+        parameters.put("positionY", readInteger(R.id.position_y));
+        parameters.put("armor", armor);
         message.setParameters(parameters);
         game.sendMessage(message);
+        
         Intent intent = new Intent(this, LocationArmoryActivity.class);
         startActivity(intent);
     }
